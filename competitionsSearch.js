3f920b5a9dbea17b921a0d6aca4bed99204dd86e
BlackScorpion18 <bs.port.mant@gmail.com>
2024-12-16 17:40:32 +0000

    competitionsSearch.html
    FALTA METER OS BOTOES DE PAGINAS NO COMPETITIONS.HTML NORMAL

--- competitionsSearch.js
+++ competitionsSearch.js
@@ -1,7 +1,5 @@
-// ViewModel KnockOut
 var vm = function () {
     console.log('ViewModel has been initiated.');
-    // local vars (set as ko observables)
     var self = this;
     self.baseUri = ko.observable('http://192.168.160.58/Paris2024/API/Competitions');
     self.displayName = 'Paris2024 Competitions List';
@@ -10,14 +8,36 @@ var vm = function () {
     self.competitions = ko.observableArray([]);
     self.sports = ko.observableArray([]); // Lista de desportos
     self.currentPage = ko.observable(1);
-    self.pagesize = ko.observable(20);
+    self.pagesize = ko.observable(330);
     self.totalRecords = ko.observable(329);
     self.hasPrevious = ko.observable(false);
     self.hasNext = ko.observable(false);
     self.sportCode = ko.observable(''); // Armazena o código do desporto selecionado
-    
+    self.searchQuery = ko.observable(""); // Observable for the search input
+
+    // Search functionality
+    self.triggerSearch = function () {
+        // Show loading modal when search starts (before AJAX request)
+        showLoading();
+
+        var query = self.searchQuery().toLowerCase(); // Convert to lowercase for case-insensitive matching
+        if (!query) {
+            self.filteredCoaches(self.coaches()); // Reset to all coaches if no search query
+            hideLoading(); // Hide loading modal immediately if no query
+        } else {
+            // Simulate asynchronous search filtering with a timeout to ensure the modal is shown before filtering starts
+            setTimeout(function () {
+                var filtered = ko.utils.arrayFilter(self.coaches(), function (coach) {
+                    return coach.Name.toLowerCase().includes(query); // Filter by coach name
+                });
+                self.filteredCoaches(filtered);
+                hideLoading(); // Hide loading modal once search is complete
+            }, 100); // small delay to ensure modal shows up
+        }
+    };
 
 
+    // Pagination helpers
     self.previousPage = ko.computed(function () {
         return self.currentPage() * 1 - 1;
     }, self);
@@ -30,7 +50,7 @@ var vm = function () {
     self.toRecord = ko.computed(function () {
         return Math.min(self.currentPage() * self.pagesize(), self.totalRecords());
     }, self);
-    self.totalPages = ko.observable(0);
+
     self.pageArray = function () {
         var list = [];
         var size = Math.min(self.totalPages(), 9);
@@ -47,16 +67,12 @@ var vm = function () {
         return list;
     };
 
-    // Função para carregar competições, com filtro opcional pelo SportId
-    self.activate = function (page, sportCode = '') {
-        console.log('CALL: getCompetitions...');
-        var composedUri = self.baseUri() + "?page=" + page + "&pageSize=" + self.pagesize();
-        if (sportCode) {
-            composedUri += "&sportCode=" + sportCode; // Adiciona o filtro de esporte
-        }
-        showLoading();
+    // Page events
+    self.activate = function (id) {
+        console.log('CALL: getCoaches...');
+        var composedUri = self.baseUri() + "?page=" + id + "&pageSize=" + self.pagesize();
         ajaxHelper(composedUri, 'GET').done(function (data) {
-            console.log("Data received:", data);
+            console.log(data);
             hideLoading();
             self.competitions(data.Competitions);
             self.currentPage(data.CurrentPage);
@@ -65,27 +81,10 @@ var vm = function () {
             self.totalRecords(data.TotalCompetitions);
         });
     };
-    
-    // Função para carregar os desportos disponíveis
-    self.loadSports = function () {
-        console.log('CALL: getSports...');
-        var sportsUri = self.baseUri() + "/Sports"; // Presume que existe uma rota para obter desportos
-        ajaxHelper(sportsUri, 'GET').done(function (data) {
-            console.log(data);
-            self.sports(data); // Supondo que a API retorna uma lista de desportos
-        });
-    };
 
-
-    self.sportCode.subscribe(function (newSportCode) {
-        console.log("Sport code changed:", newSportCode);
-        self.activate(1, newSportCode); // Passa o código do esporte para a função de carregamento
-    });
-    
-
-    // internal funcs
+    // Internal functions
     function ajaxHelper(uri, method, data) {
-        self.error(''); // clears the error msg
+        self.error('');
         return $.ajax({
             type: method,
             url: uri,
@@ -100,13 +99,6 @@ var vm = function () {
         });
     }
 
-    function sleep(milliseconds) {
-        const start = Date.now();
-        // the while loop (empty) will run for the desired duration
-        // essentially fullfilling the role of a sleep function 
-        while (Date.now() - start < milliseconds);
-    }
-
     function showLoading() {
         $("#myModal").modal('show', {
             backdrop: 'static',
@@ -116,7 +108,7 @@ var vm = function () {
     function hideLoading() {
         $('#myModal').on('shown.bs.modal', function (e) {
             $("#myModal").modal('hide');
-        })
+        });
     }
 
     function getUrlParameter(sParam) {
@@ -134,9 +126,8 @@ var vm = function () {
         }
     };
 
-    // start
+    // Start
     showLoading();
-    self.loadSports(); // Carrega os desportos
     var pg = getUrlParameter('page');
     console.log(pg);
     if (pg == undefined)
@@ -155,10 +146,3 @@ $(document).ready(function () {
 $(document).ajaxComplete(function (event, xhr, options) {
     $("#myModal").modal('hide');
 });
-
-
-
-
-
-
-
